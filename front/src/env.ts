2373115862bdf4a954265c85be96747f2db2602c
Ricardo Campos <ricardompcampos@gmail.com>
2023-10-12 16:56:14 -0300

    chore: add docker compose and update to vite

---
+++ front/src/env.ts
@@ -0,0 +1,10 @@
+declare global {
+    // eslint-disable-next-line no-unused-vars
+    interface Window {
+      config: any
+    }
+  }
+  
+  // eslint-disable-next-line import/prefer-default-export
+  export const env: Record<string, any> = { ...import.meta.env, ...window.config };
+  
---
+++ front/vite.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    {
+        name: 'build-html',
+        apply: 'build',
+        transformIndexHtml: (html) => {
+          return {
+            html,
+            tags: [
+              {
+                tag: 'script',
+                attrs: {
+                  src: '/env.js'
+                },
+                injectTo: 'head'
+              }
+            ]
+          }
+        }
+      },
+    react()
+  ],
+  build: {
+    outDir: 'build'
+  },
+  server: {
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+});
